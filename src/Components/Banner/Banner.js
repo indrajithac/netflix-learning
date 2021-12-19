ba1627430350536247c62b8a3b4a243e33a2a23f
Indrajith A C <indrajithac@gmail.com>
2021-12-19 13:06:09 +0530

    designed header,banner,rowpost

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className='content'>
+                <h1 className='title'>MOVIE NAME</h1>
+                <div className='banner_buttons'>
+                    <button className='button'>Play</button>
+                    <button className='button'>My List</button>
+                </div>
+                <h1 className='description'>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</h1>
+
+            </div>
+            <div className="fade_bottom"></div>
+        </div>
+    )
+}
+
+export default Banner
