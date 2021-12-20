838a1fbcec4947904b7a45f4e280ada02a89d5cc
Indrajith A C <indrajithac@gmail.com>
2021-12-20 20:19:33 +0530

    data of banner changing dynamically

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from 'react'
 
-import {API_KEY} from '../../constants/constants'
+import { API_KEY, imageUrl } from '../../constants/constants'
 import axios from '../../axios'
 import './Banner.css'
 
 
 function Banner() {
-    const [state, setstate] = useState(initialState)
-    useEffect(()=>{
-        axios.get(`trending/all/week?api_key=${API_KEY}`).then((response)=>{
+    const [movie, setMovie] = useState()
+
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${API_KEY}`).then((response) => {
             console.log(response.data.results[0]);
+            setMovie(response.data.results.sort(function (a, b) { return 0.5 - Math.random() })[0])
+
         })
-    },[])
-    
+    }, [])
+
     return (
-        <div className='banner'>
+
+        <div style={{ backgroundImage:  `url(${movie ? imageUrl+movie.backdrop_path : ''})` }} className='banner'>
+
             <div className='content'>
-                <h1 className='title'>MOVIE NAME</h1>
-                <div className='banner_buttons'>
+                <h1 className="title">{movie ? movie.title ? movie.title : movie.name : ''}</h1>                <div className='banner_buttons'>
                     <button className='button'>Play</button>
                     <button className='button'>My List</button>
                 </div>
-                <h1 className='description'>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</h1>
+                <h1 className='description'>{movie ? movie.overview : ""}</h1>
 
             </div>
             <div className="fade_bottom"></div>
